7ad34d979a1b9203f5281bfa699fb9b58f8d72c1
ODegtev <degtev21312@gmail.com>
2025-01-20 20:32:10 +0500

    Update pong.py

--- pong.py
+++ pong.py
@@ -24,16 +24,16 @@ class Player(GameSprite):
    # метод для управления спрайтом стрелками клавиатуры
     def update_l(self):
         keys = key.get_pressed()
-        if keys[K_w] and self.rect.x > 5:
+        if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.x < win_width - 80:
+        if keys[K_s] and self.rect.y < win_height - 155:
             self.rect.y += self.speed
     
     def update_r(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.x > 5:
+        if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.x < win_width - 80:
+        if keys[K_DOWN] and self.rect.y < win_height - 155:
             self.rect.y += self.speed
 
 racket_image = 'racket.png'
@@ -51,10 +51,19 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
-racket1 = Player(racket_image, 30, 150, 40, 200, 150)
-racket2 = Player(racket_image, 520, 150, 40, 200, 150)
+racket1 = Player(racket_image, 30, 200, 50, 150, 4)
+racket2 = Player(racket_image, 520, 200, 50, 150, 4)
 
 ball = GameSprite(ball_image, 200, 200, 50, 50, 50)
+
+font.init()
+
+font1 = font.Font(None, 35)
+lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font1.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
 
 while game:
     for e in event.get():
@@ -62,6 +71,25 @@ while game:
             game = False
     
     if not finish:
+        window.fill(back)
+        
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_y *= -1
+        
+        if ball.rect.y < 0 or ball.rect.y > win_height - 50:
+            speed_y *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, 200, 200)
+        
+        elif ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, 200, 200)
+
         racket1.update_l()
         racket2.update_r()
 
